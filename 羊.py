ff8fea39302b64fd8041500b51e8d92d6af37cd2
wyzz6 <113868662+wyzz6@users.noreply.github.com>
2022-09-19 14:52:07 +0800

    Update 羊.py

--- 羊.py
+++ 羊.py
@@ -3,7 +3,7 @@ def sheep(x,y):
     request_header = {"t":x}
     for i in range(y):
         requests.get("http://cat-match.easygame2021.com/sheep/v1/game/game_over?rank_score=1&rank_state=1&rank_time=15&rank_role=1&skin=1", headers=request_header)
-        print('第{}次通关'.format(i))
+        print('第{}次通关'.format(i+1))
     print('success!!')
 x=input('输入token:')
 y=int(input('输入次数:'))
